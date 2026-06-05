d55cfda00c7dbf4d3115e51a990850b0c8c27fd0
rahim-cmd <rahim.mohammed1@gmail.com>
2026-06-05 14:51:44 +0530

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,31 +3,26 @@ import Container from "./Container";
 
 const Footer = () => {
   return (
-    <footer className="py-20 text-white"   style={{ background: "var(--color-sage)" }}>
-
+    <footer
+      className="py-20 text-white"
+      style={{ background: "var(--color-sage)" }}
+    >
       <Container>
-
         <div className="border-t border-white/10 pt-16">
-
           <div className="text-center">
-
             <h2
               className="text-5xl mb-6"
               style={{
-                fontFamily:
-                  "Cormorant Garamond, serif"
+                fontFamily: "Cormorant Garamond, serif",
               }}
             >
-              
               Circlia
             </h2>
 
-            <p className="opacity-70">
-              A space for women to connect
-            </p>
+            <p className="opacity-70">A space for women to connect</p>
 
             <div
-  className="
+              className="
   flex
   flex-wrap
   justify-center
@@ -36,46 +31,43 @@ const Footer = () => {
   mt-10
   px-4
   "
->
+            >
+              <Link to="/" className="hover:opacity-70 transition">
+                Home
+              </Link>
 
-  <Link to="/" className="hover:opacity-70 transition">
-    Home
-  </Link>
+              <Link to="/about" className="hover:opacity-70 transition">
+                About
+              </Link>
 
-  <Link to="/about" className="hover:opacity-70 transition">
-    About
-  </Link>
+              <Link to="/journal" className="hover:opacity-70 transition">
+                Journal
+              </Link>
 
-  <Link to="/journal" className="hover:opacity-70 transition">
-    Journal
-  </Link>
+              <Link to="/contact" className="hover:opacity-70 transition">
+                Contact
+              </Link>
 
-  <Link to="/contact" className="hover:opacity-70 transition">
-    Contact
-  </Link>
+              <Link
+                to="/privacy-policy"
+                className="hover:opacity-70 transition"
+              >
+                Privacy Policy
+              </Link>
 
-  <Link
-    to="/privacy-policy"
-    className="hover:opacity-70 transition"
-  >
-    Privacy Policy
-  </Link>
-
-  <Link
-    to="/terms-and-conditions"
-    className="hover:opacity-70 transition"
-  >
-    Terms & Conditions
-  </Link>
-
-</div>
-
+              <Link
+                to="/terms-and-conditions"
+                className="hover:opacity-70 transition"
+              >
+                Terms & Conditions
+              </Link>
+            </div>
           </div>
-
+          <div className="text-center m-10 text-sm">
+            Copyright 2026. Design & Developed by Cirlcia.uk
+          </div>
         </div>
-
       </Container>
-
     </footer>
   );
 };
