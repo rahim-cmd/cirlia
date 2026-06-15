ab32cf77a1f8e874543e9dc098d2f809bc6475d9
rahim-cmd <rahim.mohammed1@gmail.com>
2026-06-15 19:27:50 +0530

    journal page updated with journalshowcase component

--- src/pages/JournalPage.jsx
+++ src/pages/JournalPage.jsx
@@ -2,7 +2,7 @@ import MainLayout from "../layout/MainLayout";
 import JournalHero from "../components/journal/JournalHero";
 import JournalPreview from "../components/journal/JournalPreview";
 import JournalBenefits from "../components/journal/JournalBenefits";
-import JournalCTA from "../components/journal/JournalCTA";
+import JournalShowcase from "../components/JournalShowcase";
 import Footer from "../components/Footer";
 
 const JournalPage = () => {
@@ -15,7 +15,7 @@ const JournalPage = () => {
 
       <JournalBenefits />
 
-      <JournalCTA />
+      <JournalShowcase />
     <Footer/>
     </MainLayout>
   );
